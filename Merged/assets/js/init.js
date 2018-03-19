bf873bfeca72b5f9d45a079fa5a66a0bb22f6ef8
Didier Krux <didier@krux.co>
2018-03-19 19:18:52 +0800

    fix commit

--- Merged/assets/js/init.js
+++ Merged/assets/js/init.js
@@ -174,24 +174,11 @@ if (isMobile) {
             currentFunction.onKeydown(e);
             currentFunction.onClick([mouseX, mouseY], e);
         }
-<<<<<<< HEAD
-        else if(e.type=='panstart') {
-            currentFunction.onKeydown(e);
-            currentFunction.onMouseDown([mouseX, mouseY], e);
-        }
-        else if(e.type=='pan') {
-            currentFunction.onDragging([mouseX,mouseY],e);
-        }
-        else if(e.type=='panend' || e.type=='pressup') {
-            currentFunction.onMouseUp([mouseX,mouseY],e);
-            //currentFunction.onMouseLeave([mouseX,mouseY],e);
-=======
         else if (e.type == 'pan') {
             currentFunction.onDragging([mouseX, mouseY], e);
         }
         else if (e.type == 'panend' || e.type == 'pressup') {
             currentFunction.onMouseUp([mouseX, mouseY], e);
->>>>>>> 566b765923645fe7d2a96c1accdfc00a24490dea
         }
     });
 }
